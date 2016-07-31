be8fa7272c571816cf6a8fe7e49b5d2af702c8d8
smalnote <smalnote@gmail.com>
2016-07-31 22:00:33 +0800

    Revert "expriment"
    This reverts commit 98d3996cd932c5be7350bc0c81d0a054205ae457.

--- src/github.com/wdeqin/flyit/flyit.go
+++ src/github.com/wdeqin/flyit/flyit.go
@@ -1,11 +1,8 @@
 package main
 
-import u "github.com/wdeqin/flyit/util"
-import "fmt"
+import "github.com/wdeqin/flyit/util"
 
 func main() {
-	u.MyPrint("1 + 1 = ", u.Add(1, 1), "\n")
-	fmt.Printf("%v\n", u.MyPrint)
-	fmt.Printf("%+v\n", u.MyPrint)
-	fmt.Printf("%#v\n", u.MyPrint)
+	println("hello, world")
+	println("1 + 1 = ", util.Add(1, 1))
 }
