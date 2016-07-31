1746c62d56ac08119d44a4746e6765941367e322
smalnote <smalnote@gmail.com>
2016-07-31 22:01:20 +0800

    Revert "Revert "expriment""
    This reverts commit be8fa7272c571816cf6a8fe7e49b5d2af702c8d8.

---
+++ src/github.com/wdeqin/flyit/util/expriment.go
@@ -0,0 +1,20 @@
+package util
+
+import "os"
+import "strconv"
+import "reflect"
+
+func Add(lhs, rhs int) int {
+	return lhs + rhs
+}
+
+func MyPrint(args ...interface{}) {
+	for _, arg := range args {
+		switch v := reflect.ValueOf(arg); v.Kind() {
+		case reflect.String:
+			os.Stdout.WriteString(v.String())
+		case reflect.Int:
+			os.Stdout.WriteString(strconv.FormatInt(v.Int(), 10))
+		}
+	}
+}
