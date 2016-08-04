1489007f39e616b8179d2b56cee4195cce5f8875
smalnote <smalnote@gmail.com>
2016-08-04 22:48:03 +0800

    web mysql ok

--- src/github.com/wdeqin/flyit/web/web.go
+++ src/github.com/wdeqin/flyit/web/web.go
@@ -8,7 +8,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"time"
 )
 
 var indexTemplate *template.Template
@@ -16,41 +15,55 @@ var db *sql.DB
 
 func init() {
 	f, err := os.Open("templates/index.html")
-	if err != nil {
-		panic(err)
-	}
+	checkErr(err)
 	b, _ := ioutil.ReadAll(f)
 	s := string(b)
 	indexTemplate = template.Must(template.New("index").Parse(s))
 
-	if db, err = sql.Open("mysql", "wdeqin:wdeqin@tcp(localhost:3306)/devdb"); err != nil {
-		panic(err)
-	}
+	db, err = sql.Open("mysql", "wdeqin:wdeqin@/devdb?charset=utf8")
+	checkErr(err)
 }
 
 type Path struct {
-	Id       int
+	Id       uint
 	Name     string
-	Time     time.Time
+	Time     string
 	Location string
 }
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		rows, err := db.Query("SELECT id, name, time, location FROM devdb.path")
-		if err != nil {
+	http.HandleFunc("/query", func(w http.ResponseWriter, r *http.Request) {
+		id := r.FormValue("id")
+		if id == "" {
 			w.WriteHeader(http.StatusInternalServerError)
-			panic(err)
+			return
 		}
+
+		var ps []Path
+
+		rows, err := db.Query("SELECT id, name, time, location FROM path")
+		checkErr(err)
 		defer rows.Close()
 
-		p := Path{}
-		if rows.Next() {
-			rows.Scan(&p.Id, &p.Name, &p.Time, &p.Location)
-			fmt.Printf("%#v\n", p)
+		for rows.Next() {
+			var id uint
+			var name string
+			var location string
+			var time string
+			rows.Scan(&id, &name, &time, &location)
+			p := Path{id, name, time, location}
+			ps = append(ps, p)
 		}
-		indexTemplate.Execute(w, p)
+
+		fmt.Println(ps)
+		indexTemplate.Execute(w, ps)
 
 	})
 	http.ListenAndServe(":8080", nil)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
